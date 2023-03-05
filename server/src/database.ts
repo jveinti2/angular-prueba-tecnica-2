124174497fca51b4afd272ca7cde16bc56b23427
Jhonatan S. Becerra <72992939+jveinti2@users.noreply.github.com>
2023-03-05 14:43:15 -0500

    Update method connection
    update connection methos to sql server

--- server/src/database.ts
+++ server/src/database.ts
@@ -10,3 +10,26 @@ pool.getConnection().then((connection) => {
 
 
 export default pool; 
+
+/* Conexión para sql server
+const sql = require("mssql");
+const sqlConfig = {
+  user: "root2",
+  password: "root2",
+  database: "newDb",
+  server: "localhost",
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: true, // change to true for local dev / self-signed certs
+  },
+};
+
+async function getConnection() {
+  const pool = await sql.connect(sqlConfig);
+  const result = await pool.request().query("SELECT 1 ");
+  console.log('Db connected');
+  console.log(result);
+}
+
+getConnection();
+*/
